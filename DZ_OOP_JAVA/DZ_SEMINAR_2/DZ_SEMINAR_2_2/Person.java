c0d38950d388d620bb637caa24f8dad47fa14384
Solomkin Alexandr <solomin1995@mail.ru>
2023-03-22 23:58:45 +0300

    DZ_OOP_JAVA_SEMINAR_2_2(Person.java): CORR

--- DZ_OOP_JAVA/DZ_SEMINAR_2/DZ_SEMINAR_2_2/Person.java
+++ DZ_OOP_JAVA/DZ_SEMINAR_2/DZ_SEMINAR_2_2/Person.java
@@ -3,6 +3,27 @@ package DZ_OOP_JAVA.DZ_SEMINAR_2.DZ_SEMINAR_2_2;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public abstract class Person {
-    private HashMap<String, Object> equipment = new HashMap<>();
+public class Person{
+    private ArrayList<Things> equipment = new ArrayList<>();
+    
+    public Person(ArrayList<Things> equipment) {
+        this.equipment = equipment;
+    }
+
+    public ArrayList<Things> getEquipment() {
+        return equipment;
+    }
+
+    public void setEquipment(ArrayList<Things> equipment) {
+        this.equipment = equipment;
+    }
+
+    public void getInfo() {
+        System.out.print("\nEquipment:");
+
+        for (Things object : equipment) {
+            System.out.printf("\nName: %s\nPurity: %d\nSizeClothes: %d\n", object.getName(), object.getPurity(),
+                    object.getSizeClothes());
+        }
+    }
 }
